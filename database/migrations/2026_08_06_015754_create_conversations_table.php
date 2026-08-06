6761fc3ad63425172250abdb4ae006e3b734010b
hasbisdqi <muhhasbiassidiqi18@gmail.com>
2026-08-06 09:00:09 +0700

    feat: create Conversation model, request classes, and migration for conversations table

---
+++ database/migrations/2026_08_06_015754_create_conversations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('conversations', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['direct', 'group'])->default('direct');
+            $table->string('name')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('direct_key')->nullable()->unique();
+            $table->foreignUuid('created_by')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete();
+            $table->timestamp('last_message_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('conversations');
+    }
+};
